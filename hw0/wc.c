df604b105f492ff14075cd10fb90ca47b56c2fa7
Vapour101 <vapour101@gmail.com>
2015-07-24 11:11:56 +0200

    Refractored wc.c

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 void wc(FILE* ofile, FILE* infile, char* inname)
 {
-}
-
-int main (int argc, char* argv[])
-{
-    FILE* file;
-    file = fopen(argv[1], "r");
-    
-    if (file == NULL)
+    if (infile == NULL)
     {
         printf("No such file.\n");
-        return 1;
+        exit(1);
     }
     
     int wordCount = 0, lineCount = 0, byteCount = 0;
     fpos_t fileStart;
-    fgetpos(file, &fileStart);
+    fgetpos(infile, &fileStart);
     
     char temp[50];
-    for (;fscanf(file, "%s", temp) != EOF; wordCount++)
+    for (;fscanf(infile, "%s", temp) != EOF; wordCount++)
     {}
     
-    fsetpos(file, &fileStart);
+    fsetpos(infile, &fileStart);
     char buffer[100];
     size_t num;
-    while (num = fread(buffer, 1, 100, file))
+    while (num = fread(buffer, 1, 100, infile))
     {
         byteCount += num;
         
@@ -39,13 +33,17 @@ int main (int argc, char* argv[])
             break;
     }
     
+    fprintf(ofile, "%i\t%i\t%i\t%s\n", lineCount, wordCount, byteCount, inname);
+}
+
+int main (int argc, char* argv[])
+{
+    FILE* file;
+    file = fopen(argv[1], "r");
+    
+    wc(stdout, file, argv[1]);
+    
     fclose(file);
-    
-    /*printf("Line count: %i\n", lineCount);
-    printf("Word count: %i\n", wordCount);
-    printf("Byte count: %i\n", byteCount);*/
-    
-    printf("%i\t%i\t%i\t%s\n", lineCount, wordCount, byteCount, argv[1]);
     
     return 0;
 }
