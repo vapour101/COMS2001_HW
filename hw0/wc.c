6a8312c931bf42ccc39590c7d2d005cb3009efe3
Vapour101 <vapour101@gmail.com>
2015-08-02 13:08:14 +0200

    wc now gives properly spaced columns.

--- hw0/wc.c
+++ hw0/wc.c
@@ -3,13 +3,98 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-void printLine(int lines, int words, int bytes, char* file)
+#define max(a,b) (a>b?a:b)
+
+void wc(FILE*, char*);
+void addLine(int, int, int, char*);
+void printLines();
+
+typedef struct{
+    int lines;
+    int words;
+    int bytes;
+    char* file;
+} Line;
+
+Line* output;
+int iOutput;
+int maxWidth;
+
+int main (int argc, char* argv[])
 {
-    printf("%i\t%i\t%i\t%s\n", lines, words, bytes, file);
+    if (argc == 1)
+        return 0;
+    
+    maxWidth = 0;
+    iOutput = (argc == 2) ? 1 : argc;
+    
+    FILE* file;
+    output = (Line*) malloc(sizeof(Line) * iOutput);
+    
+    if (iOutput > 1)
+    {
+        output[iOutput-1].lines = 0;
+        output[iOutput-1].words = 0;
+        output[iOutput-1].bytes = 0;
+        output[iOutput-1].file = "total";
+    }
+    
+    for (int i = 1; i < argc; i++)
+    {
+        file = fopen(argv[i], "r");
+        wc(file, argv[i]);
+        fclose(file);
+    }
+    
+    printLines();
+    
+    return 0;
 }
 
-void wc(FILE* infile, char* inname, int* tLines, int* tWords, int* tBytes)
+void addLine(int lines, int words, int bytes, char* file)
+{
+    static int i = 0;
+    
+    char buffer[50];
+    
+    sprintf(buffer, "%i", lines);
+    maxWidth = max(maxWidth, strlen(buffer));
+    
+    sprintf(buffer, "%i", words);
+    maxWidth = max(maxWidth, strlen(buffer));
+    
+    sprintf(buffer, "%i", bytes);
+    maxWidth = max(maxWidth, strlen(buffer));
+    
+    output[i].lines = lines;
+    output[i].words = words;
+    output[i].bytes = bytes;
+    output[i].file = file;
+    
+    i++;
+    
+    if (iOutput > 1)
+    {
+        output[iOutput-1].lines += lines;
+        output[iOutput-1].words += words;
+        output[iOutput-1].bytes += bytes;
+    }
+}
+
+void printLines()
+{
+    char outputFormat[256];
+    sprintf(outputFormat, "%%%ii %%%ii %%%ii %%s\n", maxWidth, maxWidth, maxWidth);
+    
+    //printf("DEBUG\n%s\nEND DEBUG\n", outputFormat);
+    
+    for (int i = 0; i < iOutput; i++)
+        printf(outputFormat, output[i].lines, output[i].words, output[i].bytes, output[i].file);
+}
+
+void wc(FILE* infile, char* inname)
 {
     if (infile == NULL)
     {
@@ -28,7 +113,7 @@ void wc(FILE* infile, char* inname, int* tLines, int* tWords, int* tBytes)
     fsetpos(infile, &fileStart);
     char buffer[100];
     size_t num;
-    while (num = fread(buffer, 1, 100, infile))
+    while ((num = fread(buffer, 1, 100, infile)))
     {
         byteCount += num;
         
@@ -40,26 +125,5 @@ void wc(FILE* infile, char* inname, int* tLines, int* tWords, int* tBytes)
             break;
     }
     
-    *tLines += lineCount;
-    *tWords += wordCount;
-    *tBytes += byteCount;
-    
-    printLine(lineCount, wordCount, byteCount, inname);
-}
-
-int main (int argc, char* argv[])
-{
-    FILE* file;
-    int lines = 0, words = 0, bytes = 0;
-    
-    for (int i = 1; i < argc; i++)
-    {
-        file = fopen(argv[i], "r");
-        wc(file, argv[i], &lines, &words, &bytes);
-        fclose(file);
-    }
-    
-    printLine(lines, words, bytes, "total");
-    
-    return 0;
+    addLine(lineCount, wordCount, byteCount, inname);
 }
