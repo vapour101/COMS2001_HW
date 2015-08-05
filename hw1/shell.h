718dbff09d3178e94e4e23078d839354a702210d
Vapour101 <vapour101@gmail.com>
2015-08-05 17:57:45 +0200

    Fixed shell.c and shell.h
    shell.h:
    	variable was declared as id_t instead of pid_t.
    
    shell.c:
    	Added signal.h to the includes, this allows the POSIX kill() function to work.

--- hw1/shell.h
+++ hw1/shell.h
@@ -7,7 +7,7 @@
 int shell_terminal; //File descripter for the terminal.
 int shell_is_interactive; //1 if shell_terminal is a valid terminal. 0 otherwise
 struct termios shell_tmodes; //terminal options for the shell
-id_t shell_pgid; //The shell's process id
+pid_t shell_pgid; //The shell's process id
 int shell(int argc, char *argv[]);
 
 #endif
