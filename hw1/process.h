0f43a15d9b29116a864a8a149efa3b7f89c14d17
Vapour101 <vapour101@gmail.com>
2015-08-06 21:22:30 +0200

    Started HW1

--- hw1/process.h
+++ hw1/process.h
@@ -6,25 +6,25 @@
 #include <termios.h>
 #include <unistd.h>
 
-typedef struct process
+typedef struct
 {
-  char** argv;
-  int argc;
-  pid_t pid;
-  char completed;
-  char stopped;
-  char background;
-  int status;
-  struct termios tmodes;
-  int stdin, stdout, stderr;
-  struct process* next;
-  struct process* prev;
+    char** argv;
+    int argc;
+    pid_t pid;
+    char completed;
+    char stopped;
+    char background;
+    int status;
+    struct termios tmodes;
+    int stdin, stdout, stderr;
+    struct process* next;
+    struct process* prev;
 } process;
 
-process* first_process; //pointer to the first process that is launched */
+process* first_process; //pointer to the first process that is launched
 
 void launch_process(process* p);
 void put_process_in_background (process* p, int cont);
 void put_process_in_foreground (process* p, int cont);
 
-#endif
+#endif //_PROCESS_H_
